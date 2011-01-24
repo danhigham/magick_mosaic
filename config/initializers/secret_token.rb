cf554133c884b7039a7e3ee695a264b53d943ed0
Dan Higham <dan@dan-VirtualBox.(none)>
2011-01-24 13:41:58 +0000

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagickMosaic::Application.config.secret_token = 'bd98caf8559d00eaaf6f0057d7efcaa0ebe45a4d08166467200c00eee5c8acac7c10495d037c5975b8a48bd122202ea1ffe06be6043ea1f73771703e7082ff10'
